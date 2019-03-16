693a363372fae03edd6aa12e2ddbce3227b30b15
Ryan Pethel <arpethel@gmail.com>
2019-03-16 14:12:44 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,7 +1,16 @@
+require "pry"
+
 def reformat_languages(languages)
   new_hash = {}
+  # new_lang_values = languages.keys
 
-  languages.each do |style_k, style_v|
-    new_lang_keys = languages.keys
+  languages.each do |lang_k, lang_v|
+    new_hash.merge!(lang_v)
   end
+
+  # new_hash.each do |languages, values|
+  #   puts values
+  # end
+
+  return new_hash
 end
