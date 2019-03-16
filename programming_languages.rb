eca4661f9d66478e2aaac569c6b43c6c5919c77d
Ryan Pethel <arpethel@gmail.com>
2019-03-16 14:35:05 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,26 +1,14 @@
 require "pry"
 
 def reformat_languages(languages)
-  new_hash = {}
-  # new_lang_values = languages.keys
-
-  languages.each do |lang_k, lang_v|
-    new_hash.merge!(lang_v)
+  def reformat_languages(languages)
+  new_hash_v = {}
+  languages.each do |k, v|
+    binding.pry
+    new_hash_v[v] = {}
   end
-  new_hash.each do |languages, values|
-      binding.pry
-      # add languages keys as values of the new_hash
-      # merging into the new_hash basically ".shift"s the languages hash
-      # add :style to ..
-      # basically, new_hash[:languages][:style] = {:oo}
-      # if type == "interpreted", set style for hash as ":oo"
-      # if languages == :javascript
-      #
-      # end
 
-
-    end
-    return new_hash
+end
 end
 
 # puts reformat_languages(languages)
