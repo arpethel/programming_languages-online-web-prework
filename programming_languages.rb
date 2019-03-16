592c2d458823685a549a53e93834070831247c2d
Ryan Pethel <arpethel@gmail.com>
2019-03-16 16:30:20 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,26 +2,28 @@ require "pry"
 
 def reformat_languages(languages)
 
-  # new_hash_v = {}
-  # languages.each do |k, v|
-  #   v.each do |lang, type|
-  #     binding.pry
-  #     new_hash_v[lang] = {}
-  #     new_hash_v[:style] ||= []
-  #     new_hash_v[:style] << k
-  #   end
-  #   return new_hash_v
-  # end
-  new_hash = {}
-  languages.each do |lang_k, lang_v|
-    new_hash.merge!(lang_v)
-    binding.pry
-    # new_hash[lang_k][:style]
-    # new_hash[:style] ||= []
-    # new_hash[:style] << lang_k
+  new_hash_v = {}
+  languages.each do |k, v|
+    v.each do |lang, type|
+      binding.pry
+      new_hash_v[lang] = {}
+      new_hash_v[:style] ||= []
+      new_hash_v[:style] << k
+    end
   end
-
-  return new_hash
+  return new_hash_v
 end
+  # new_hash = {}
+  # languages.each do |lang_k, lang_v|
+  #   new_hash.merge!(lang_v)
+  #   # binding.pry
+  # end
+
+  # new_hash.each do |lang, values|
+  #   
+  # end
+
+#   return new_hash
+# end
 
 # puts reformat_languages(languages)
